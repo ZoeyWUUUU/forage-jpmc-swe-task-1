1ccde41372d1e76cd70a4b625acd740d341e70d6
吴梓漩 <apple@wuzixuandeMacBook-Pro.local>
2024-02-10 12:01:21 -0500

    main function updated

--- client3.py
+++ client3.py
@@ -54,8 +54,11 @@ if __name__ == "__main__":
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
         """ ----------- Update to get the ratio --------------- """
+        stock_price_dic = {}
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
+            stock_price_dic[stock] = price
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(price, price))
+
+        print("Ratio %s" % getRatio(stock_price_dic.get(quotes[0][0]), stock_price_dic.get(quotes[1][0])))
