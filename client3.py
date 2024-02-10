74059904c6c1098ea3ecf01cf970b07d0085b031
吴梓漩 <apple@wuzixuandeMacBook-Pro.local>
2024-02-10 11:45:57 -0500

    getDataPoint avgFormula change

--- client3.py
+++ client3.py
@@ -21,7 +21,6 @@
 import json
 import random
 import urllib.request
-import statistics
 
 # Server API URLs
 QUERY = "http://localhost:8080/query?id={}"
@@ -36,7 +35,7 @@ def getDataPoint(quote):
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = statistics.mean(bid_price, ask_price)
+    price = (bid_price + ask_price) / 2
     return stock, bid_price, ask_price, price
 
 
